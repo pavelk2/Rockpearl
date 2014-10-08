b5a65f966bbe77efa7c3b2fe9cbd28b77f61ab2b
Pavel Kucherbaev <pavel.kucherbaev@gmail.com>
2014-10-08 17:16:45 +0200

    no message

--- rockpearl/settings.py
+++ rockpearl/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+SESSION_COOKIE_SECURE = True
 from settings_database import *
 from settings_credentials import *
 
@@ -37,7 +38,7 @@ MARBLE_3D_ENLARGE_POLYGON = 100
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 TEMPLATE_DEBUG = True
 
