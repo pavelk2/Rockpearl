77fffe5a6444a398e8f2b4c9a126404bc28458d6
Pavel Kucherbaev <pavel.kucherbaev@gmail.com>
2014-10-10 16:47:28 +0200

    no message

--- rocklistener/views.py
+++ rocklistener/views.py
@@ -8,8 +8,9 @@ from django.shortcuts import redirect, HttpResponseRedirect
 
 import logging
 log = logging.getLogger(__name__)
-
+job_id = 8
 crowdcafe = CrowdCafe()
+
 @csrf_exempt
 def webhook_dropbox(request):
     '''Respond to the webhook verification (GET request) by echoing back the challenge parameter.'''
@@ -27,8 +28,23 @@ def webhook_dropbox(request):
             for path, metadata in updates:
                 log.debug('path and metadata')
                 log.debug(path)
-                log.debug(dbuser.getDirectLink(path))
-                log.debug(metadata)
+                
+                # create unit
+                rockpearl_url = 'http://rockpearl.crowdcafe.io/'
+                image_url = rockpearl_url+'rocklistener/getMediaLink/'+str(uid)+'/?'+path
+
+                filename = path[path.rfind('/')+1:len(path)]
+                rest_path = path[:path.rfind('/')]
+                folder = rest_path[rest_path.rfind('/')+1:len(rest_path)]
+                unit_data = {
+                        'image_id' : image.id,
+                        'uid': uid,
+                        'block_title' : folder,
+                        'image_filename': filename,
+                        'url': image_url
+                    }
+                unit = crowdcafe.createUnit(job_id, unit_data)
+                log.debug(unit)
                 #if crowdcafe.createUnit(job_id, unit_data) in [200,201]:
     	return HttpResponse(status=200)
 
