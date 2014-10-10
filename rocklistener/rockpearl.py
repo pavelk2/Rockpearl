106421da49f7a8b47065df299dd1d50b0b77f7b2
Pavel Kucherbaev <pavel.kucherbaev@gmail.com>
2014-10-10 17:27:29 +0200

    no message

--- rocklistener/rockpearl.py
+++ rocklistener/rockpearl.py
@@ -15,13 +15,13 @@ class Rockpearl:
 		return media['url']
 
 	def publishImage(self,path, metadata):
-		if metadata and metadata['mime_type'] == 'image/jpeg'
+		if metadata and metadata['mime_type'] in ['image/jpeg','image/png']:
 			uid = self.dropbox_user.uid
 			log.debug('path and metadata')
 			log.debug(path)
 			log.debug(metadata)
 			
-			new_metadata = self.dropbox_user.client.metadata(path,include_media_info = True)
+			new_metadata = self.dropbox_user.client.metadata(path, include_media_info = True)
 			log.debug(new_metadata)
 
 			# create unit
@@ -29,6 +29,7 @@ class Rockpearl:
 			filename = path[path.rfind('/')+1:len(path)]
 			rest_path = path[:path.rfind('/')]
 			folder = rest_path[rest_path.rfind('/')+1:len(rest_path)]
+
 			unit_data = {
 			'image_id' : 123,
 			'uid': uid,
@@ -38,5 +39,5 @@ class Rockpearl:
 			'url': image_url}
 			unit = crowdcafe.createUnit(self.job_id, unit_data)
 			log.debug(unit)
-			
-			return unit
+
+		return True
